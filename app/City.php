3ba850cbae12249025fe3b60ff1c8d0e0cae21fa
Thales Nathan <thcmatias@gmail.com>
2020-04-29 18:45:04 -0300

    Defining models

--- app/City.php
+++ app/City.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    protected $fillable = [
+        'state_id',
+        'name'
+    ];
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
 }
--- app/State.php
+++ app/State.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class State extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'acronym'
+    ];
+
+    public function cities()
+    {
+        return $this->hasMany(City::class);
+    }
 }
