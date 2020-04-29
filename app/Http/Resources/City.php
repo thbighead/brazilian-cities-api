e553c798263b4e2ac7b172c7d6c386151926b3c1
Thales Nathan <thcmatias@gmail.com>
2020-04-29 12:34:44 -0300

    Defining resources structure

--- app/Http/Resources/City.php
+++ app/Http/Resources/City.php
@@ -14,6 +14,10 @@ class City extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'state' => State::collection($this->whenLoaded('state'))
+        ];
     }
 }
--- app/Http/Resources/State.php
+++ app/Http/Resources/State.php
@@ -14,6 +14,11 @@ class State extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'acronym' => $this->acronym,
+            'name' => $this->name,
+            'cities' => City::collection($this->whenLoaded('cities'))
+        ];
     }
 }
