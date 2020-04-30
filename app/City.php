bcd9bc84be4f0aa7e6bcd7c41a23701feea760fc
Thales Nathan <thcmatias@gmail.com>
2020-04-29 23:40:28 -0300

    Adding models PHP doc through IDE Helper package
    php artisan ide-helper:model -W -R

--- app/City.php
+++ app/City.php
@@ -4,6 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\City
+ *
+ * @property int $id
+ * @property int $state_id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\State $state
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City whereStateId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\City whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class City extends Model
 {
     protected $fillable = [
--- app/State.php
+++ app/State.php
@@ -4,6 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\State
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $acronym
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\City[] $cities
+ * @property-read int|null $cities_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State whereAcronym($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\State whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class State extends Model
 {
     protected $fillable = [
